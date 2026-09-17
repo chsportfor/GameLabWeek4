291cad92f219aaabda9228d6b2afa2968636b6dd
chsportfor <chsportfor@gmail.com>
2026-09-18 00:44:36 +0900

    Objectiterator 추가

---
+++ EngineLib/Core/Object/Objectiterator.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "Object.h"
+
+TSparseArray<UObject*> UObject::GUObjectArray;
+
+template<typename TObject>
+class FObjectIterator
+{
+private:
+	int32 CurrentIndex;
+
+	FObjectIterator& operator++()
+	{
+		++CurrentIndex;
+		AdvanceToNextValidObject();
+		return *this;
+	}
+
+	void AdvanceToNextValidObject() {
+		// GObjObjects
+		// Obj->IsA<TObject>()
+	}
+};
