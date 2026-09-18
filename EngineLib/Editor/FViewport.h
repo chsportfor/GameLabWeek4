06e98dae672712676918315f8f00765e67f50bd0
HYOJEONGYANG <jjjjjeong0213@gmail.com>
2026-09-18 12:02:48 +0900

    feat: FViewport 클래스 초기 구현
    뷰포트 정보 갖게끔 설정!
    메인 우선 합치기 위해 초기본 임시로 올립니다~

---
+++ EngineLib/Editor/FViewport.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "Core/Core.h"
+#include <d3d11.h>
+
+struct FEditorViewportClient;
+
+enum class ELevelViewportType : uint8 {
+	Perspective,
+	Top,
+	Bottom,
+	Left,
+	Right,
+	Front,
+	Back
+};
+
+
+struct FViewport {
+public:
+	void SetRect(float x, float y, float w, float h);
+
+	void SetViewport(D3D11_VIEWPORT inViewport) { ViewportInfo = inViewport; };
+	const D3D11_VIEWPORT& GetViewport() const { return ViewportInfo; }
+
+	float GetAspect() const {
+		if (ViewportInfo.Height == 0) return 1.0f;
+		return ViewportInfo.Width / ViewportInfo.Height;
+	}
+
+	bool IsHover(int32 x, int32 y) const;
+
+private:
+	D3D11_VIEWPORT ViewportInfo = {};
+	FEditorViewportClient* Client = {};
+};
