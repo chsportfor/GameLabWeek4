85226afba50b90b874e0a81df0929e65f7c70b13
chsportfor <chsportfor@gmail.com>
2026-09-18 13:58:28 +0900

    Update UStaticMeshComponent.h

--- EngineLib/Engine/Components/UStaticMeshComponent.h
+++ EngineLib/Engine/Components/UStaticMeshComponent.h
@@ -3,12 +3,13 @@
 #include "UMeshComponent.h"
 #include "Core/IO/JsonUtil.h"
 #include "Core/Object/ObjectFactory.h"
+#include "Engine/Assets/StaticMesh.h"
 
 class UStaticMeshComponent : public UMeshComponent
 {
 	DECLARE_OBJECT(UStaticMeshComponent, UMeshComponent)
 	DECLARE_SERIALIZATION()
 
-	//UStaticMesh* StaticMesh; 
+	FStaticMesh* StaticMesh; 
 	void Serialize(bool bIsLoading, json::JSON Handle);
 };
