266fdc86ca18f5d02945a4fda1efd5c3e2e584e7
chsportfor <chsportfor@gmail.com>
2026-09-18 18:23:15 +0900

    UStaticMesh 추가

---
+++ EngineLib/Engine/Components/UStaticMesh.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Engine/Assets/StaticMesh.h"
+#include "Core/Object/Object.h"
+
+class UStaticMesh : public UObject
+{
+	FStaticMesh* StaticMeshAsset;
+	
+	const FString& GetAssetPathFileName() {
+		return StaticMeshAsset->PathFileName;
+	}
+
+	void SetStaticMeshAsset(FStaticMesh* InStaticMesh) {
+		StaticMeshAsset = InStaticMesh;
+	}
+};
